f6c5c12ac0153c39ea666647b2a15c604bdd73e0
root <alexballe35@gmail.com>
2020-09-21 10:56:08 +0200

    remove console logs"

--- src/redux/reducers/mail.js
+++ src/redux/reducers/mail.js
@@ -6,21 +6,18 @@ const INITIAL_STATE = {
 const mail = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'MAIL_SUCCESS':
-            console.log("here");
             return {
                 ...state,
                 loading: false,
                 error: ""
             }
         case 'MAIL_LOADING':
-            console.log("here1");
             return {
                 ...state,
                 loading: true,
                 error: ""
             }
         case 'MAIL_ERROR':
-            console.log("here2");
             return {
                 ...state,
                 loading: false,
